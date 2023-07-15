76a20b3984db60af6ecd7a57f4899f997a4c28e6
Jayne Valverde <jaynevalverde@gmail.com>
2023-07-15 18:42:06 -0500

    dependencies added to index.js/connect to database

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // TODO: Include packages needed 
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
+// string styling
+const chalk = require('chalk');
 
-// TODO: Connection to MySQL
+// FIGFont in Javascript
+const figlet = require('figlet');
 
+// Print MySQL rows to the console 
+require('console.table');
+
+// TODO: Connect to database
+const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        passowrd: '',
+        database: 'employee_db'
+    });
 
 // TODO: Welcome Banner 
 
