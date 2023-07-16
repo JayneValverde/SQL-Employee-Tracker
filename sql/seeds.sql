63c0b6cd76374421d22063f7566f092012399d1b
Jayne Valverde <jaynevalverde@gmail.com>
2023-07-15 19:55:22 -0500

    seed.sql started

---
+++ sql/seeds.sql
@@ -0,0 +1,20 @@
+USE employee_db;
+
+INSERT INTO department(name)
+VALUES 
+('Sales'),
+('Support'),
+('Marketing'),
+('Development');
+
+INSERT INTO role 
+(title, salary, department_id)
+
+VALUES
+(),
+
+INSERT INTO employee 
+(first_name, last_name, role_id, manager_id)
+
+VALUES
+(),
