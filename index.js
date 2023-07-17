3823a30f50eb303f141e10e7f2a08faf0633b65d
Jayne Valverde <jaynevalverde@gmail.com>
2023-07-16 19:14:14 -0500

    first prompt added / select statmets

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 // string styling
-const chalk = require('chalk');
+const chalk = import('chalk');
 
 // FIGFont in Javascript
 const figlet = require('figlet');
@@ -29,6 +29,110 @@ console.log(``);
 console.log(chalk.orange.bold(`=======================================================================================================`))
 
 // TODO: Create prompts / arrays 
+
+const firstPrompt = () => {
+
+    inquirer
+        .prompt({
+            name: 'choices',
+            type: 'list',
+            message: 'PLEASE SELECT A MENU OPTION...',
+            choices: [
+                "View Employees",
+                "View All Roles",
+                "View All Departments",
+                "Update Employee Role",
+                "Add Employee",
+                "Add Roll", 
+                "Add New Department", 
+                chalk.orange('Update Employee Manager'),
+                chalk.orange('Delete Employee'),
+                chalk.orange('Delete Role'),
+                chalk.orange('Delete Department'),
+                'Exut Menu',
+            ],
+        }).then((answers) => {
+            const { choices } = answers; 
+
+            if (choices === 'View All Employees') {
+                viewAllEmployees();
+            }
+            if (choices === 'View All Rolles') {
+                viewAllRoles();
+            }
+            if (choices === 'View All Departments') {
+                viewAllDepartments();
+            }
+            if (choices === 'View Employees By Manager') {
+                viewEmployeesByManager();
+            }
+            if (choices === 'Update Employee Role') {
+                updateEmployeeRole();
+            }
+            if (choices === 'Add New Employee') {
+                addNewRole();
+            }
+            if (choices === 'Add New Roll') {
+                addNewDepartment();
+            }
+            if (choices === 'Add New Department') {
+                addNewDepartment();
+            }
+            if (choices === 'Update Employee Managers') {
+                updateEmployeeManagers();
+            }
+            if (choices === 'Delete Employee') {
+                deleteEmployee();
+            }
+            if (choices === 'Delete Role') {
+                deleteRole();
+            }
+            if (choices === 'Delete Department') {
+                deleteDepartment();
+            }
+            if (choices === 'Exit Menu') {
+                console.log('Logged out! Type npm start to login')
+                connection.end();
+            }
+        });
+};
+
+// SQL SELECT * FROM statments for choices 
+const viewAllEmployees = () => {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    firstPrompt();
+}
+
+const viewAllRoles = () => {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err; 
+        console.table(res);
+    })
+    firstPrompt();
+}
+
+const viewAllDepartments = () => {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err; 
+        console.table(res); 
+    })
+    firstPrompt();
+}
+
+const viewEmployeesByManager = () => {
+    const query = 'SELECT * FROM employee ORDER BY manager_id DESC';
+    connection.query(query, (err, res) => {
+        if (err) throw err; 
+        console.table(res);
+    })
+    firstPrompt();
+}
 
 
 // TODO: VIEW Functions 
